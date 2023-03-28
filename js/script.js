d3193211004dfb0213151a568abcf310ceafcee4
Iryna Pec <118050604+irapets777@users.noreply.github.com>
2023-03-28 19:44:09 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,10 +23,6 @@ class Clients {
     }
 }
 
-let client1 = new Clients("Iren", "123", 7000);
-let client2 = new Clients("Vasa", "456", 8000);
-let client3 = new Clients("Nazar", "789", 9000);
-
 ///5================================================================
 
 class Bank {
@@ -61,15 +57,13 @@ class Bank {
       }
     }
   }
-  
-  const bank = new Bank("My Bank");
 
 //============================================================================================
 
 document.addEventListener("DOMContentLoaded", () =>{
-    console.log(client1);
-    console.log(client2);
-    console.log(client3);
+    let client1 = new Clients("Iren", "123", 7000);
+    let client2 = new Clients("Vasa", "456", 8000);
+    let client3 = new Clients("Nazar", "789", 9000);
     console.log(client1.getBalance(), client2.getBalance(), client3.getBalance());
     if (client1.withdraw(200), client2.withdraw(300), client3.withdraw(440)) {
         console.log("Гроші успішно знято  рахунку");
@@ -77,6 +71,7 @@ document.addEventListener("DOMContentLoaded", () =>{
         console.log("Недостатньо коштів!");
     }
     console.log(client1.getBalance(), client2.getBalance(), client3.getBalance());
+    const bank = new Bank("My Bank");
     bank.addCustomer("Maria", 2000);
     bank.addCustomer("Ivan", 1500);
     console.log(bank.customers);
